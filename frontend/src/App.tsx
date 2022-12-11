ef1ca64ea681799b7c198f641dbd50e2444247e3
Cariny <perezcariny@gmail.com>
2022-12-10 16:24:05 -0800

    feat add header and register components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home'; 
 import Login from './pages/Login';
 import Register from './pages/Register'; 
+
 
 function App() {
   return (
     <>
       <Router> 
         <div className='container'>
+            <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/login' element={<Login/>}/>
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,8 +1,26 @@
+import {useState} from 'react'; 
+import { FaUser } from 'react-icons/fa';
+
+
 const Register = () => {
+	const [formData, setFormData] = useState({
+		name: '',
+		email: '',
+		password: '',
+		confirmPassword: ''
+	})
+
+	const {name, email, password, confirmPassword} = formData; 
+
 	return (
-		<div>
-			Register
-		</div>
+		<>
+		<section className='heading' >
+			<h1>
+				<FaUser/> Register 
+			</h1>
+			<p>Please create an account</p>
+		</section>
+		</>
 	)
 }
 
